0e6e1acc2e5941ffe3801976a6b11588403c9fda
devsadieste <116064293+devsadieste@users.noreply.github.com>
2022-10-28 11:12:35 +0200

    Add files via upload
    fichier parametre

---
+++ admin/parametre/parametre.php
@@ -0,0 +1,9 @@
+<?php
+    //fichier contenant les differents parametres permettant de se connecter a la base de donnee 
+    //le fichier est relie sur chaque page
+    $hote='hangartofficiel.mmi-lepuy.fr';
+    $utilisateur='ermenut_hangart';
+    $mdp='hangartgroupe5';
+    $port=3306;
+    $nombase='hangart';
+?>
